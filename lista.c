a51db53f5885249a8affac6d4820a96a2d778a0c
Helton Ferreira Varela <helton1@outlook.com>
2016-09-03 15:17:46 -0400

    1.2
    Função INSERIR(incompleta)

--- lista.c
+++ lista.c
@@ -10,18 +10,18 @@ struct estruturaNo{
   struct No *proxPtr;//ponteiro que aponta para o próximo Nó
 };
 
-//Definição de nomes
+//Definição de nome
 typedef struct estruturaNo No;// define o tipo estruturaNo para No
-typedef No *NoPtr;// define o tipo No para um ponteiro
 
 //Protótipos
+void inserir(No **inicioPtr, char informacao);//ponteiro que aponta para o inicio(como se fosse uma nova cabeca) e a informacao a ser armazenada
 
 int main(){
   system("clear");
 
   int op;
   char item;
-  NoPtr cabeca = NULL;//cria uma variável dinamica(ponteiro) que aponta para NULL
+  No *cabeca = NULL;//cria uma variável dinamica(ponteiro) que aponta para NULL
 
   //Menu
   printf("Digite sua escolha:\n");
@@ -45,4 +45,16 @@ int main(){
   }
 
   return 0;
+}
+
+void inserir(No **inicioPtr, char valor){//ponteiro para ponteiro, pois aponta para o ponteiro *novo
+  No *novoPtr, *anteriorPtr, *atualPtr;
+  novoPtr = malloc(sizeof(No));//aloca um bloco de bytes consecutivos na memória RAM do computador e devolve o endereço desse bloco
+  if(novoPtr!=NULL){//se tiver um endereco armazenado, significa que há espaco disponivel, entao
+    novoPtr->dado = valor;//o ponteiro que aponta para um novo no, recebe um valor e o atribui no campo dado
+    novoPtr->proxPtr = NULL;//o ponteiro que aponta para um novo no, recebe NULL para o campo que aponta para o proximo ponteiro
+
+    anteriorPtr = NULL; //o ponteiro que aponta para o no anterior, recebe como endereco NULL
+    atualPtr = *inicioPtr;//o ponteiro do proprio no atual, recebe como endereco o conteudo de inicioPtr(lembre-se que o conteudo de inicioPtr tbm eh um endereco, pois inicioPtr eh um ponteiro para ponteiro)
+  }
 }
